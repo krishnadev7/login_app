d42279379b8f7b858859fe04185fbc49a5bc1500
krishnadev7 <krishnadevv12@gmail.com>
2023-04-10 22:08:23 +0530

    Protected Routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,45 @@
-import {BrowserRouter,Route,Routes} from 'react-router-dom';
-import { Username,Password,Register,Profile,Recovery,Reset,PageNotFound } from './components';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  Username,
+  Password,
+  Register,
+  Profile,
+  Recovery,
+  Reset,
+  PageNotFound,
+} from './components';
+import { AuthorizeUser, ProtectRoute } from './middleware/auth';
 
 function App() {
-  return <>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' exact element={<Username/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/password' element={<Password/>}/>
-        <Route path='/recovery' element={<Recovery/>}/>
-        <Route path='/reset' element={<Reset/>}/>
-        <Route path='/profile' element={<Profile/>}/>
-        <Route path='*' element={<PageNotFound/>}/>
-      </Routes>
-    </BrowserRouter>
-  </>
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<Username />} />
+          <Route path='/register' element={<Register />} />
+          <Route
+            path='/password'
+            element={
+              <ProtectRoute>
+                <Password />
+              </ProtectRoute>
+            }
+          />
+          <Route path='/recovery' element={<Recovery />} />
+          <Route path='/reset' element={<Reset />} />
+          <Route
+            path='/profile'
+            element={
+              <AuthorizeUser>
+                <Profile />
+              </AuthorizeUser>
+            }
+          />
+          <Route path='*' element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/middleware/auth.jsx
@@ -0,0 +1,15 @@
+import { Navigate } from 'react-router-dom';
+import { useAuthStore } from '../store/store';
+
+export const AuthorizeUser = ({ children }) => {
+  const token = localStorage.getItem('token');
+  if (!token) return <Navigate to='/' replace={true}></Navigate>;
+  return children;
+};
+
+
+export const ProtectRoute = ({children}) => {
+  const {username: {username}} = useAuthStore(state => state.auth);
+  if(!username) return <Navigate to='/' replace={true}></Navigate>
+  return children;
+}
